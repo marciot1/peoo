c9922f77f0adbb714af472acd1cbcd9748044e00
MARCIO TARGINO <105668116+marciot1@users.noreply.github.com>
2022-05-27 18:54:22 +0000

    exec.cs

---
+++ exec.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program{
+  public static void Main(){
+  int = int.Parse(Console.ReadLine());
+    switch mes {
+      case 1 : Console.Writeline("Jan"); break;
+      case 2 : Console.WriteLine("Fev"); break;
+      case 3 : Console.WriteLine("Mar"); break;
+      case 4 : Console.WriteLine("Abr"); break;
+      case 5 : Console.WriteLine("Mai"); break;
+      case 6 : Console.WriteLine("Jun"); break;
+      case 7 : Console.WriteLine("Jul"); break;
+      case 8 : Console.WriteLine("Ago"); break;
+      case 9 : Console.WriteLine("Set"); break;
+      case 10 : Console.WriteLine("Out"); break;
+      case 11 : Console.WriteLine("Nov"); break;
+      case 12 : Console.WriteLine("Dez"); break;
+      default : Conesole.WriteLine("valor inválido"); break;
+  }
+}
