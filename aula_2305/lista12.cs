5fa1bee5cf5a6ca117d3c5bb7c78841cf08f37a0
MARCIO TARGINO <105668116+marciot1@users.noreply.github.com>
2022-06-04 22:52:26 +0000

    lista12.cs

---
+++ aula_2305/lista12.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+  public static void Main(){
+   int a = int.Parse(Console.ReadLine());
+   int b = int.Parse(Console.ReadLine());
+   int c = ( a + b);
+    Console.WriteLine($"O resultado da operação é " + c);
+  }
+}
